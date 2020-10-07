3be673077354a3b67130b2d787bcbb0eeaad3b92
Rob Squires <rsquires@sans.org>
2020-10-07 14:53:59 +0100

    finished

---
+++ src/scripts.js
@@ -0,0 +1,53 @@
+const quoteBox = document.getElementById("random-quote-box");
+const button = document.getElementById("button");
+const quoteList = document.getElementById("quote-list");
+
+const BASE_URL = "https://programming-quotes-api.herokuapp.com";
+
+const spinner = `<div id="spinner"></div>`;
+
+function quoteComponent(quote, author) {
+  return `
+    <q class="quote">${quote}</q>
+    <br /><br />
+    <em class="author"> - ${author}</em>
+    `;
+}
+
+async function getFromApi(url) {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+button.addEventListener("click", async function () {
+  try {
+    quoteBox.innerHTML = spinner;
+    const quote = await getFromApi(`${BASE_URL}/quotes/random/lang/en`);
+    quoteBox.innerHTML = quoteComponent(quote.en, quote.author);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+async function loadQuotesIntoList() {
+  try {
+    quoteList.innerHTML = spinner;
+    const quotes = await getFromApi(`${BASE_URL}/quotes`);
+
+    let htmlList = ``;
+
+    quotes.forEach((quote) => {
+      htmlList += `<li>${quoteComponent(quote.en, quote.author)}</li>`;
+    });
+
+    quoteList.innerHTML = htmlList;
+  } catch (error) {
+    console.log(error);
+  }
+}
+loadQuotesIntoList();
